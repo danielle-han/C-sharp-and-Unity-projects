d4991fde86bc25b92b76f81d2010127d3976d712
danielle-han <61366948+danielle-han@users.noreply.github.com>
2021-08-12 15:13:15 -0700

    Added BoolLogicAssignment folder and file

---
+++ basic_C#_programs/myConsoleApplication/BoolLogicAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BoolLogicAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please enter True or False");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age > 15 && dui == false && ticket <= 3);
+
+            Console.Read();
+        }
+    }
+}
