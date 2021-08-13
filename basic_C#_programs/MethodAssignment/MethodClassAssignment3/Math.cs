5b4e154eff12e8a42c6c5cb65fec8e2c5e2d5fb0
danielle-han <61366948+danielle-han@users.noreply.github.com>
2021-08-13 16:19:51 -0700

    Added MethodClassAssignment3 folder and files

---
+++ basic_C#_programs/MethodAssignment/MethodClassAssignment3/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment3
+{
+    public class Math
+    {
+        public void method1(int integer, int number)
+        {
+            int total =  integer * 2;
+            Console.WriteLine(total);
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ basic_C#_programs/MethodAssignment/MethodClassAssignment3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            math.method1(3, 2);
+            math.method1(integer: 7, number: 10);
+            Console.Read();
+        }
+    }
+}
