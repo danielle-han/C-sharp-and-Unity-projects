c5e68bfe74ba7528ed3cee93a8d6ae9df8c471b8
danielle-han <61366948+danielle-han@users.noreply.github.com>
2021-08-13 16:13:27 -0700

    Added MethodClassAssignment2 folder and files

---
+++ basic_C#_programs/MethodAssignment/MethodClassAssignment2/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment2
+{
+    public class Math
+    {
+        public int operation(int integer, int number = 1)
+        {
+            return integer * number;
+        }
+    }
+}
---
+++ basic_C#_programs/MethodAssignment/MethodClassAssignment2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            //user input
+            Console.WriteLine("enter a number:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter another number if you wish:");
+            try
+            {
+                int userNum2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(math.operation(userNum, userNum2));
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine(math.operation(userNum));
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
