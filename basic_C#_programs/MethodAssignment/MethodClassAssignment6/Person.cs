9cd742aaa6c5b2029f600b09f42a2d30f827ec4c
danielle-han <61366948+danielle-han@users.noreply.github.com>
2021-08-14 12:02:50 -0700

    Added MethodClassAssignment6 folder and files

---
+++ basic_C#_programs/MethodAssignment/MethodClassAssignment6/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment5
+{
+    public abstract class Person
+    {
+        //properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //method
+        public abstract void SayName();
+    }
+}
