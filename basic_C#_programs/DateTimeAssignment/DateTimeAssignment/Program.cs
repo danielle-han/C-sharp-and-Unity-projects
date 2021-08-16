343b3a5427e219f7bbb278aa0e6f9c377b661e5c
danielle-han <61366948+danielle-han@users.noreply.github.com>
2021-08-16 12:34:35 -0700

    Added DateTime assignment folder and files

---
+++ basic_C#_programs/DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //current date and time
+            Console.WriteLine(DateTime.Now);
+
+            //asking for number
+            Console.WriteLine("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            TimeSpan time = new TimeSpan(num, 0, 0);
+            TimeSpan currentTime = DateTime.Now.TimeOfDay;
+
+            time += currentTime;
+
+            //time in 'num' hours:
+            Console.WriteLine("The time in {0} hours is: " + time.ToString(), num);
+
+            Console.Read();
+        }
+    }
+}
--- basic_C#_programs/TwentyOne/Program.cs
+++ basic_C#_programs/TwentyOne/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOne
 {
@@ -10,36 +11,40 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            //object with values: shorthand - initializes object with values
-            //Card card = new Card() { Face = "King", Suit = "Spades" };
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
 
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
 
-            //TwentyOneGame game = new TwentyOneGame();
-            //game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
-            //game.ListPlayers();
-            //Console.ReadLine();
-            //game.Dealer = "Jesse";
-            //game.Name = "TwentyOne";
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
 
-            //Game game = new TwentyOneGame();
-            //game.Players = new List<Player>();
-            //Player player = new Player();
-            //player.Name = "Jesse";
-            //game += player;
-            //game -= player;
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.IsActivelyPlaying = true;
 
-            Deck deck = new Deck();
-            //deck.Shuffle();
-            //Shuffle(deck: deck, times: 3) //named parameters.
+                while (player.IsActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
 
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            Console.ReadLine();
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.Read();
         }
 
-        
+
     }
 }
+
+//File I/O (using system.io)
+//string text = "Here is some text.";
+//File.WriteAllText(@"C:\Users\hand1\Desktop\log.txt", text); //@ - read string exactly as string - if not there would need to use \ as escape char for \ (\\)
+//string text = File.ReadAllText("C:\\Users\\hand1\\Desktop\\log.txt");
